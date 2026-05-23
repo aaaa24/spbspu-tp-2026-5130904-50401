ae49c89d45a930dfc8fc1bf78e07a4a81bbd8099
aaaa24 <chernov.arseniy@yandex.ru>
2026-05-23 23:12:41 +0300

    add skipping bad lines

--- chernov.arseniy/T2/main.cpp
+++ chernov.arseniy/T2/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <sstream>
 #include <vector>
 
@@ -11,8 +12,17 @@ int main()
   std::vector< chernov::DataStruct > data;
   using iit_t = std::istream_iterator< chernov::DataStruct >;
   using oit_t = std::ostream_iterator< chernov::DataStruct >;
+  std::istream & input = std::cin;
+  std::ostream & output = std::cout;
+  std::streamsize max_streamsize = std::numeric_limits< std::streamsize >::max();
 
-  std::copy(iit_t{std::cin}, iit_t{}, std::back_inserter(data));
+  while (input) {
+    std::copy(iit_t{input}, iit_t{}, std::back_inserter(data));
+    if (!input.eof()) {
+      input.clear();
+      input.ignore(max_streamsize, '\n');
+    }
+  }
   std::sort(data.begin(), data.end());
-  std::copy(std::begin(data), std::end(data), oit_t{std::cout, "\n"});
+  std::copy(std::begin(data), std::end(data), oit_t{output, "\n"});
 }
