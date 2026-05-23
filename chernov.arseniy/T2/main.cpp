92e0d4101eb22e3e6f2cf2709ed1fc80a745c9c6
aaaa24 <chernov.arseniy@yandex.ru>
2026-05-23 19:02:22 +0300

    define namespace chernov

--- chernov.arseniy/T2/main.cpp
+++ chernov.arseniy/T2/main.cpp
@@ -1,2 +1,34 @@
+#include <iostream>
+
+namespace chernov {
+  struct DataStruct {
+    long long key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+
+  struct LongLongIO {
+    long long & ref;
+  };
+
+  struct UnsignedLongLongIO {
+    unsigned long long & ref;
+  };
+
+  struct StringIO {
+    std::string & ref;
+  };
+
+  struct LabelIO {
+    std::string exp;
+  };
+
+  struct DelimiterIO {
+    char exp;
+  };
+}
+
 int main()
-{}
+{
+
+}
