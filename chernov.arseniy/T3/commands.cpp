f0708fd9447df3807688cf897113005937197dba
aaaa24 <chernov.arseniy@yandex.ru>
2026-06-01 19:00:21 +0300

    add cmdArea

--- chernov.arseniy/T3/commands.cpp
+++ chernov.arseniy/T3/commands.cpp
@@ -1,6 +1,7 @@
 #include "commands.hpp"
 
 #include <limits>
+#include <string>
 
 void chernov::runCommands(std::istream & input, std::ostream & output,
   const std::unordered_map< std::string, cmd_t > & cmds, std::vector< Polygon > & polygons)
@@ -24,4 +25,26 @@ void chernov::runCommands(std::istream & input, std::ostream & output,
   }
 
   runCommands(input, output, cmds, polygons);
+}
+
+void chernov::cmdArea(std::istream & input, std::ostream & output, std::vector< Polygon > & polygons)
+{
+  std::string param = "";
+  if (!(input >> param)) {
+    return;
+  }
+
+  size_t num_of_vertex = 0;
+  try {
+    size_t num_of_vertex = std::stoull(param);
+  } catch (...) {
+    if (param == "EVEN" || param == "ODD") {
+
+    } else if (param == "MEAN") {
+
+    } else {
+      output << "<INVALID COMMAND>\n";
+      return;
+    }
+  }
 }
